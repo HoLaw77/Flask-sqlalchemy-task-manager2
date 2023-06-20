7528e9d85f8f7fed19c7682c52195a9d2d3db731
HoLaw77 <>
2023-06-20 06:52:57 +0000

    comment out create_tables

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ from taskmanager import app
 
 if __name__ == "__main__":
 
-    # def create_tables(self):
-    #     with app.app_context():
-    #         db.create_all()
-    # create_tables(db)
+     #def create_tables(self):
+      #   with app.app_context():
+       #      db.create_all()
+     #create_tables(db)
     app.run(
         host=os.environ.get("IP"),
         port=os.environ.get("PORT"),
