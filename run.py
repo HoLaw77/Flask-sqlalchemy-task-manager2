181b991cdba4032b9cc875bf06aa0416f8db3b5a
HoLaw77 <>
2023-06-20 06:46:14 +0000

    create database

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import os
-
 from taskmanager import app
 
+
 if __name__ == "__main__":
+
+    # def create_tables(self):
+    #     with app.app_context():
+    #         db.create_all()
+    # create_tables(db)
     app.run(
         host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG"),
+        port=os.environ.get("PORT"),
+        debug=os.environ.get("DEBUG")
     )
